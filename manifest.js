733e302fc6946a8dc3153aebf6566fca0e0d1820
DEMENSDEUM-ACER\Demensdeum <demensdeum@gmail.com>
2024-11-20 11:15:58 +0400

    Struct

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+    "manifestVersion" : 1,
+    "sdkVersion" : "1.0.0",
+    "runtime" : "JavaScript",
+    "title" : "Whitelist",
+    "info" : "GramEXT extension for whitelisting contacts",
+    "buildVersion" : "1.0.0",
+    "license" : "MIT",
+    "access" : [
+        "Input Messages"
+    ],
+    "executables" : [
+        "Whitelist.js",
+        "main.js"
+    ]
+}
